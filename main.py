c446ba739927cc54a0382e9d574d2b675640cccf
Harry Tran <css012013@coderacademy.edu.au>
2020-09-11 15:24:01 +1000

    added prettytable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from prettytable import PrettyTable
 
 def get_definition(word):
     link = f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}"
@@ -18,10 +19,22 @@ while True:
 
     try:
         for num in range(len(temp[0]["meanings"])):
-            print("definition: " + temp[0]["meanings"][num]["definitions"][0]["definition"])
-            print("example: " + temp[0]["meanings"][num]["definitions"][0]["example"])
+            def1 = temp[0]["meanings"][num]["definitions"][0]["definition"]
+            # print("definition: " + def1)
+            exp1 = temp[0]["meanings"][num]["definitions"][0]["example"]
+            # print("example: " + exp1)
+            sym1 = False
             if "synonyms" in temp[0]["meanings"][num]["definitions"][0].keys():
-                print("synonyms: ", temp[0]["meanings"][num]["definitions"][0]["synonyms"])
-            print("*********")
+                sym1 = temp[0]["meanings"][num]["definitions"][0]["synonyms"]
+                # print("synonyms: ", sym1)
+            d = PrettyTable()
+            d.field_names = ["-", "details"]
+            d.add_row(["Definition", def1])
+            d.add_row(["",""])
+            d.add_row(["Example", exp1])
+            if sym1:
+                d.add_row(["Synonym", sym1])
+            print(d)
     except ValueError:
         print(f"{user_word} is not a word. Try again")
+
